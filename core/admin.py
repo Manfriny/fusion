c6194797812f1d5085bc21dc5465bf7422a88ce6
Manfriny <manfriny.pereira@grupotecagro.com>
2020-11-06 16:50:58 -0300

    Aula 53

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cargo, Servico, Funcionario
 
 # Register your models here.
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['cargo', 'criado', 'modificado', 'ativo']
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'criado', 'modificado', 'ativo')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'criado', 'modificado', 'ativo')
--- fusion/settings.py
+++ fusion/settings.py
@@ -127,4 +127,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
-MNEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
